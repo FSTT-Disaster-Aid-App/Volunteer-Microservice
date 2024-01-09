caa2eea9846dde7670dac811571b994a7e19c468
AYGA2K <garouatayoub@gmail.com>
2024-01-10 00:41:18 +0100

    show error

--- src/main/java/ma/fstt/controller/SkillController.java
+++ src/main/java/ma/fstt/controller/SkillController.java
@@ -58,7 +58,7 @@ public class SkillController {
 			return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("data", savedSkill));
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(Map.of("error", "Error while processing the request"));
+					.body(Map.of("error", e.getMessage()));
 		}
 	}
 
